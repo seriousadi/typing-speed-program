03bd1d17a2880288e683bc8db99d66f7e825717c
Aditya Kumar <aaz22hsns@gmail.com>
2023-07-20 12:42:59 +0530

    Improve some functionalites and code

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
 from tkinter import *
 import datetime
 
-l = datetime.datetime.now()
+startin_time = datetime.datetime.now()
 
 window = Tk()
 font = ("monospace", 25,)
 
-str_to_type = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porttitor luctus dolor, " \
-              "ut tempor eros faucibus eu. Fusce finibus nec elit vitae tincidunt. \n" \
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porttitor luctus dolor, " \
-              "ut tempor eros faucibus eu. Fusce finibus nec elit vitae tincidunt. \n"
+str_to_type = """English texts for beginners to practice reading and comprehension online 
+and for free. Practicing your comprehension of written English will both improve your 
+vocabulary and understanding of grammar and word order. The texts below are designed to 
+help you develop while giving you an instant evaluation of your progress.
+Prepared by experienced English teachers, the texts, articles and conversations are
+ brief and appropriate to your level of proficiency. Take the multiple-choice quiz 
+ following each text, and you'll get the results immediately. You will feel both challenged 
+ and accomplished! You can even download (as PDF) and print the texts and exercises. 
+ It's enjoyable, fun and free. Good luck!"""
 
 str_to_type = str_to_type.split()
 typed_word = ""
 typed_str = []
+uncorrected_entries = 0
+current_time = 0
 
 
-def cal_speed(total_typed_words, uncorrected_errors):
+def cal_speed(total_typed_words, uncorrected_errors, time_in_minutes: int = 1):
     all_typed_entries = 0
     for n in total_typed_words:
         all_typed_entries += len(n)
-    gross_wpm = (all_typed_entries / 5) / 1
-    net_wpm = (gross_wpm - (uncorrected_errors / 1)) / 1
+    gross_wpm = (all_typed_entries / 5) / time_in_minutes
+    net_wpm = (gross_wpm - (uncorrected_errors / time_in_minutes)) / time_in_minutes
     return net_wpm, gross_wpm
 
 
-def sayhi(some):
-    global typed_word, typing_area, uncorrected_entries
-    current_time = (datetime.datetime.now() - l).seconds
+def typing_letters(typed_key):
+    global typed_word, typing_area, uncorrected_entries, current_time
+    current_time = (datetime.datetime.now() - startin_time).seconds
     current_time = int(current_time)
     if current_time > 60:
         window.quit()
-    uncorrected_entries = 0
-    key_touched = some.char
-
-    if key_touched == ' ':
-        typed_str.append(typed_word)
-        typed_word = ""
-        typing_area.delete(0, END)
 
     else:
-        typed_word += key_touched
+        uncorrected_entries = 0
+        key_touched = typed_key.char
 
-    if key_touched == "\x08":
-        typed_word = typed_word[:-2]
-    elif key_touched == '\x7f':
-        typed_word = ""
-        typing_area.delete(0, END)
+        if key_touched == ' ':
+            typed_str.append(typed_word)
+            typed_word = ""
+            typing_area.delete(0, END)
 
-    for n in range(len(typed_str)):
-        if typed_str[n] != str_to_type[n]:
-            uncorrected_entries += 1
+        else:
+            typed_word += key_touched
+
+        if key_touched == "\x08":
+            typed_word = typed_word[:-2]
+        elif key_touched == '\x7f':
+            typed_word = ""
+            typing_area.delete(0, END)
+
+        for n in range(len(typed_str)):
+            if typed_str[n] != str_to_type[n]:
+                uncorrected_entries += 1
 
 
 # Text area
@@ -62,10 +71,13 @@ text.pack()
 typing_area = Entry(width=20, font=font, justify="center")
 typing_area.pack()
 
-# binding typing area with space and any keypress event
-typing_area.bind("<KeyPress>", func=sayhi)
+# binding typing areca with space and any keypress event
 
+typing_area.bind("<KeyPress>", func=typing_letters)
 window.mainloop()
 
-net_wpm, gross_wpm = cal_speed(uncorrected_errors=uncorrected_entries, total_typed_words=typed_str)
-print(net_wpm, gross_wpm)
+if current_time > 60:
+    net_wpm, gross_wpm = cal_speed(uncorrected_errors=uncorrected_entries,
+                                   total_typed_words=typed_str,
+                                   time_in_minutes=round(current_time / 60))
+    print(net_wpm, gross_wpm)
